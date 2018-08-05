5e780b81d5d792819dca2c2619ec72665f323517
sam2310 <amir_sam2310@yahoo.com>
2018-08-05 23:24:04 +0800

    First commit

---
+++ resources/views/admin/posts/edit.blade.php
@@ -0,0 +1,78 @@
+@extends('layouts.admin')
+
+
+@section('content')
+
+	<h1>Edit Post</h1>
+
+
+	<div class="row"> 
+
+		<div class="col-sm-3">
+
+			<img src="{{$post->photo->file}}" class="img-reponsive"></img>	
+
+		</div>	
+
+
+		<div class="col-sm-6"> 
+
+		{!! Form::model($post,['method'=>'PATCH', 'action'=> ['AdminPostsController@update',$post->id],'files'=>true]) !!}
+		
+		
+		      <div class="form-group">
+		             {!! Form::label('title', 'Title:') !!}
+		             {!! Form::text('title', null, ['class'=>'form-control'])!!}
+		       </div>
+		
+		
+		       <div class="form-group">
+		       		 {!! Form::label('category_id', 'Category:') !!}
+		       		 {!! Form::select('category_id', $categories , null, ['class'=>'form-control'])!!}
+		       </div>
+
+
+		       	<div class="form-group">
+		             {!! Form::label('photo_id', '  Photo:') !!}
+		             {!! Form::file('photo_id', null, ['class'=>'form-control'])!!}
+		        </div>
+		       
+				
+				<div class="form-group">
+				     {!! Form::label('body', '  Description:') !!}
+				     {!! Form::textarea('body', null, ['class'=>'form-control','rows'=>3])!!}
+				</div>
+
+				<div class="form-group">
+                	{!! Form::submit('Updated', ['class'=>'btn btn-primary col-sm-6' ]) !!}
+             	</div>
+
+				
+
+		 {!! Form::close() !!}
+
+			 {!! Form::open(['method'=>'DELETE', 'action'=> ['AdminPostsController@destroy', $post->id]]) !!}
+
+                 <div class="form-group">
+                    {!! Form::submit('Delete user', ['class'=>'btn btn-danger col-sm-6']) !!}
+                 </div>
+
+            {!! Form::close() !!}
+
+		</div>
+
+	</div>
+
+
+		<div class="row">
+
+			@include('includes.form_error')	 
+		
+		</div>
+	
+
+
+
+
+
+@stop
---
+++ resources/views/admin/posts/index.blade.php
@@ -0,0 +1,64 @@
+@extends('layouts.admin')
+
+
+
+@section('content')
+
+    <h1> Posts </h1>
+
+     <table class="table">
+       <thead>
+         <tr>
+             <th>Id</th>
+             <th>Photo</th>
+             <th>Owner</th>
+             <th>Category</th>
+             <th>Title</th>
+             <th>Body</th>
+             <th>Post link</th>
+             <th>Comments</th>
+             <th>Created at</th>
+             <th>Update</th>
+         </tr>    
+        </thead>
+        <tbody>
+
+    @if ($posts)
+    	
+		@foreach ($posts as $post)
+			
+			<tr>	
+				<td>{{$post->id}}</td>
+                <td><img height="50" src="{{$post->photo ? $post->photo->file : 'http://placehold.it/400x400'}}" alt=""></td>
+				<td><a href="{{route('admin.posts.edit',$post->id)}}">{{$post->user->name}}</a></td>
+				<td>{{$post->category ? $post->category->name : 'Uncategorized'}}</td>
+				<td>{{$post->title}}</td>
+				<td>{{str_limit($post->body ,30)}}</td>
+                <td><a href="{{ route('home.post',$post->slug) }}">View Post</a></td>
+                <td><a href="{{ route('admin.comments.index') }}">View Comments</a></td>
+				<td>{{$post->created_at}}</td>
+				<td>{{$post->updated_at}}</td>
+
+
+			</tr>
+
+		  @endforeach
+
+        @endif
+
+       </tbody>
+    </table>
+        
+        <div class="row">
+
+            <div class="col-sm-6 col-sm-offset-5">
+                
+                {{$posts->render()}}
+                
+
+            </div>
+        </div>
+
+
+
+@stop   
